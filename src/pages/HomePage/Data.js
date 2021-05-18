a14e9cd1f730bc6b48276b5079425632c28c009b
Robson Vinicius <robsondgn@outlook.com>
2021-05-18 02:18:32 -0300

    Home Page route, styles, data

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,15 @@
+export const homeObjOne = {
+  lightBg: false, 
+  primary: false,     
+  imgStart: '',
+  lightTopLine: true,
+  lightTextDesc: true, 
+  buttonLabel: 'Get Started',
+  description: 'We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goals', 
+  headline: 'Lead Generation Specialist for Online Business', 
+  lightText: true, 
+  topLine: 'Marketing Agency', 
+  img: require('../../images/scrum.svg'),
+  alt: 'Image cade você?',
+  start: ''
+}
